be6e0109f306d897d4f405894a700165f404fc6a
j <chuthiminhhang_t62@hus.edu.vn>
2019-03-06 21:52:01 +0700

    Modify square

--- ch1/square.c
+++ ch1/square.c
@@ -5,7 +5,7 @@ int main(int n) {
     printf("Enter number n: ");
     scanf("%d", &n);
 
-    if (sqrt(n) == round(sqrt(n)))
+    if (sqrt(n) == (n / (sqrt(n)))) // Another alternative is to compare with rounded n squared
         printf("%d is a perfect square", n);
     else
         printf("%d is not a perfect square", n);
